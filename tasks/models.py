871fb4b185f5f5722523d14d9e0675eaf3179c60
inetti <inetmail84@gmail.com>
2019-02-16 16:13:22 +0300

    Add Task and Tag models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return "/tag/{}".format(self.slug)
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    parent = models.ForeignKey("self", null=True, blank=True, related_name="children", on_delete=models.CASCADE)
+    is_done = models.BooleanField(default=False)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    date_of_ending = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "{} ({}: {})".format(self.title, 
+                                    self.user.username, 
+                                    self.date_of_creation.isoformat(timespec='seconds'))
+
+    def get_absolute_url(self):
+        return "{}/{}".format(self.user.name, self.slug)
+
