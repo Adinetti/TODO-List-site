ddbc733114a5873ab6b3eba57f2c8f129cfa303c
inetti <inetmail84@gmail.com>
2019-02-16 20:16:23 +0300

    Add bootstrap and some new views with templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wellcome)
+    path('', views.wellcome, name="wellcome"),
+    path('logout', views.logout_user, name="logout"),
+    path('login', views.login_user, name="login"),
+    path('<str:username>', views.index, name="index") 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def wellcome(request):
-    return render(request, 'tasks/base.html')
+    if request.user.is_authenticated:
+        return redirect("/{}".format(request.user.username))
+    return render(request, 'tasks/wellcome.html')
+
+def index(request, username):
+    if not request.user.is_authenticated:
+        return redirect("/")
+    return render(request, 'tasks/index.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/{}".format(username))
+    return render(request, 'tasks/login.html')
